6f202f0893e440903aae726b4780c2222ea8440b
josh <aaronbaon1@gmail.com>
2024-11-21 21:16:29 +0800

    added navigation bar

---
+++ app/Views/components/nav.php
@@ -0,0 +1,20 @@
+<nav>
+    <div>
+        <a href="" class="nav-logo">Elliuro</a>
+    </div>
+    <div class="nav-links">
+        <a href="">EXPLORE PROPERTIES</a>
+        <a href="">LIST PROPERTY</a>
+        <a href="" class="nav-link-singup">SIGNUP</a>
+        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+            </svg></a>
+        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                class="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                <path fill-rule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+            </svg></a>
+    </div>
+</nav>
