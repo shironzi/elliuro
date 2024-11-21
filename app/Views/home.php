a7050db9cad5279cd7bf0ba50c2ea2855db7992d
josh <aaronbaon1@gmail.com>
2024-11-21 22:35:01 +0800

    add banner section with slider and update styles

--- app/Views/home.php
+++ app/Views/home.php
@@ -10,7 +10,24 @@
 
 <body>
     <?php include __DIR__ . '/components/nav.php'; ?>
-    <h1>Hello</h1>
+    <div class="banner">
+        <div>
+            <h1>Covelly Estates</h1>
+            <p>Your Gateway to Luxury Living and
+                <span>Exceptional Real Estate Opportunities</span>
+            </p>
+        </div>
+        <div class="banner-slider">
+            <img class="banner-slider-image" src="<?= base_url('assets/house.png') ?>" alt="">
+            <img class="banner-slider-image" src="<?= base_url('assets/house.png') ?>" alt="">
+            <img class="banner-slider-image" src="<?= base_url('assets/house.png') ?>" alt="">
+            <img class="banner-slider-image" src="<?= base_url('assets/house.png') ?>" alt="">
+            <img class="banner-slider-image" src="<?= base_url('assets/house.png') ?>" alt="">
+            <img class="banner-slider-image" src="<?= base_url('assets/house.png') ?>" alt="">
+            <img class="banner-slider-image" src="<?= base_url('assets/house.png') ?>" alt="">
+
+        </div>
+    </div>
     <?php include __DIR__ . '/components/footer.php'; ?>
 </body>
 
