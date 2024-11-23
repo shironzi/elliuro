b2495e2a4843865c54a2521213b69e5fc2bd62e2
josh <aaronbaon1@gmail.com>
2024-11-23 23:17:48 +0800

    refactor authentication views and styles for login and registration

--- app/Views/login.php
+++ app/Views/login.php
@@ -13,23 +13,12 @@
     <?php include __DIR__ . '/components/nav.php'; ?>
 
     <div class="auth-container">
-        <h1>SIGN UP</h1>
+        <h1>SIGN IN</h1>
 
         <form action="" class="auth-form">
-            <input type="text" name="name" id="name" placeholder="NAME" required>
             <input type="email" name="email" id="email" placeholder="EMAIL" required>
             <input type="password" name="password" id="password" placeholder="PASSWORD" required>
-            <input type="password" name="confirm-password" id="confirm-password" placeholder="CONFIRM PASSWORD"
-                required>
-            <div class="auth-form-custom">
-                <input type="date" name="birthday" id="birthday" placeholder="BIRTHDAY" required>
-                <select name="gender" id="gender" required>
-                    <option value="">Select Gender</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                </select>
-            </div>
-            <button type="submit">SIGN UP</button>
+            <button type="submit">SIGN IN</button>
         </form>
     </div>
 
--- app/Views/register.php
+++ app/Views/register.php
@@ -4,11 +4,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('styles/auth.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('styles/main.css') ?>">
     <title>Document</title>
 </head>
 
 <body>
-    <h1>Register</h1>
+    <?php include __DIR__ . '/components/nav.php'; ?>
+
+    <div class="auth-container">
+        <h1>SIGN UP</h1>
+
+        <form action="" class="auth-form">
+            <input type="text" name="name" id="name" placeholder="NAME" required>
+            <input type="email" name="email" id="email" placeholder="EMAIL" required>
+            <input type="password" name="password" id="password" placeholder="PASSWORD" required>
+            <input type="password" name="confirm-password" id="confirm-password" placeholder="CONFIRM PASSWORD"
+                required>
+            <div class="auth-form-custom">
+                <input type="date" name="birthday" id="birthday" placeholder="BIRTHDAY" required>
+                <select name="gender" id="gender" required>
+                    <option value="">Select Gender</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+            </div>
+            <button type="submit">SIGN UP</button>
+        </form>
+    </div>
+
+
+
+    <?php include __DIR__ . '/components/footer.php'; ?>
 </body>
 
 </html>
