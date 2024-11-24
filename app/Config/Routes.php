0ae8ef9c409be11aa6a95ce74d02c0cc80f7ba4d
josh <aaronbaon1@gmail.com>
2024-11-24 16:43:40 +0800

    refactor property routes and enhance property form view with improved styles and structure

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,5 +10,5 @@ $routes->get('/login', 'Auth::login');
 $routes->get('/register', 'Auth::register');
 $routes->get('/property', 'Properties::details');
 $routes->get('/properties', 'Properties::index');
-$routes->get('/property-form', 'ListingProperty::propertyForm');
-$routes->get('/listing-property', 'ListingProperty::index');
+$routes->get('/property-form', 'Properties::propertyForm');
+$routes->get('/listing-property', 'Properties::index');
