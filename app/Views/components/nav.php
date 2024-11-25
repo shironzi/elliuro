b2bf56b29f8900d0e5681f18cf5d361af75a92bb
josh <aaronbaon1@gmail.com>
2024-11-25 12:22:24 +0800

    show login/signup links based on user authentication status

--- app/Views/components/nav.php
+++ app/Views/components/nav.php
@@ -5,16 +5,23 @@
     <div class="nav-links">
         <a class="nav-links-effect" href="<?= base_url('/properties') ?>">EXPLORE PROPERTIES</a>
         <a class="nav-links-effect" href="<?= base_url('/listing-property') ?>">LIST PROPERTY</a>
-        <a href="<?= base_url('/register') ?>" class="nav-link-singup">SIGNUP</a>
-        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-heart-fill" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
-            </svg></a>
-        <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                class="bi bi-person-circle" viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                <path fill-rule="evenodd"
-                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
-            </svg></a>
+
+        <?php if (session()->get('is_logged_in')): ?>
+
+
+            <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-heart-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+                </svg></a>
+            <a href=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                    class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                    <path fill-rule="evenodd"
+                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                </svg></a>
+        <?php else: ?>
+            <a href="<?= base_url('/register') ?>" class="nav-link-singup">SIGNUP</a>
+            <a href="<?= base_url('/login') ?>" class="nav-link-singup">LOGIN</a>
+        <?php endif; ?>
     </div>
 </nav>
