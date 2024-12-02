fde2959e6e2c827afd2fba62ed090bb0e932d574
josh <aaronbaon1@gmail.com>
2024-12-02 20:34:10 +0800

    added FeatureProperties component

---
+++ frontend/src/components/home/FeatureProperties.tsx
@@ -0,0 +1,37 @@
+function FeatureProperties() {
+
+
+    return(
+        <div className="container">
+            <h1>FEATURED PROPERTIES</h1>
+            <div>
+            <div>
+                <h1>Luxe Haven Residences</h1>
+
+                <p>Nestled in the heart of Manila, this exceptional property combines modern elegance with unparalleled comfort, offering spacious interiors, top-of-the-line amenities, and breathtaking views – the perfect retreat for refined living.</p>
+            
+                <div>
+                <div>
+                    <h6>Swimming Pool</h6>
+                    <h6>Rooms</h6>
+                </div>
+
+                <div>
+                    <h6>Car Port</h6>
+                    <h6>Bathrooms</h6>
+                </div>
+                </div>
+
+                <div>
+                    <h1>PHP 5,000,000.00</h1>
+                    <a href="">VIEW LISTING</a>
+                </div>
+            </div>
+            <img src="../../../public/images/featured-property.png" alt="" />
+            </div>
+            
+        </div>
+    )
+}
+
+export default FeatureProperties;
