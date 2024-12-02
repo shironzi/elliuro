98b4cd22b9999c4da0903ff682b0143234ca9e8e
josh <aaronbaon1@gmail.com>
2024-12-02 20:36:48 +0800

    add FeatureProperties component to Home

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,9 +1,11 @@
 import Banner from './Banner'
+import FeatureProperties from './FeatureProperties'
 
 function Home() {
   return (
     <>
       <Banner />
+      <FeatureProperties/>
     </>
   )
 }
