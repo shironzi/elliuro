6c4b2c7775ad5b453aae8bf4ed447feac839e48d
josh <aaronbaon1@gmail.com>
2024-12-04 17:26:11 +0800

    improved layout and styling

--- frontend/src/components/home/FeaturePropertyDesign1.tsx
+++ frontend/src/components/home/FeaturePropertyDesign1.tsx
@@ -8,20 +8,26 @@ function FeaturePropertyDesign1(props: {
   image: string
 }) {
   return (
-    <div className="container flex justify-between">
-      <div>
-        <h1>{props.title}</h1>
-        <p>{props.desc}</p>
-        <div>
-          <h1>{props.hasSwimmingPool ? 'Swimming Pool' : <></>}</h1>{' '}
-          <h1>{props.noOfRooms} Rooms</h1>
+    <div className="container w-4/5 mx-auto">
+        <div className="flex justify-between mx-auto gap-x-28">
+            <div className="flex flex-col gap-y-6">
+            <h1 className="text-3xl text-beige-400 tracking-wider">{props.title}</h1>
+            <p className="text-primary-400 tracking-wide text-xl">{props.desc}</p>
+            <div className="flex justify-between">
+                <h1>{props.hasSwimmingPool ? 'Swimming Pool' : <></>}</h1>{' '}
+                <h1>{props.noOfRooms} Rooms</h1>
+            </div>
+            <div className="flex justify-between">
+                <h1>{props.hasCarPort ? 'Car Port' : <></>}</h1>{' '}
+                <h1>{props.noOfBathrooms} Bathrooms</h1>
+            </div>
+            </div>
+            <img
+            src={props.image}
+            alt=""
+            style={{ width: '634px', height: '422.718px' }}
+            />
         </div>
-        <div>
-          <h1>{props.hasCarPort ? 'Car Port' : <></>}</h1>{' '}
-          <h1>{props.noOfBathrooms} Bathrooms</h1>
-        </div>
-      </div>
-      <img src={props.image} alt="" />
     </div>
   )
 }
