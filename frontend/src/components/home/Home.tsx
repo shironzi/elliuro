e018f1902e9462be4982f15d0df651177447a928
josh <aaronbaon1@gmail.com>
2024-12-05 23:59:08 +0800

    add InvestWithUs component

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,11 +1,13 @@
 import Banner from './Banner'
 import FeatureProperties from './FeatureProperties'
+import InvestWithUs from './InvestWithUs'
 
 function Home() {
   return (
     <div className="bg-darkGray-400">
       <Banner />
       <FeatureProperties />
+      <InvestWithUs />
     </div>
   )
 }
