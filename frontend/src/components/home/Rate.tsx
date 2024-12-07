cd9327b1289fa4c6d766f3c5ceccfa8453f9effd
josh <aaronbaon1@gmail.com>
2024-12-07 13:19:37 +0800

    add more layouts

--- frontend/src/components/home/Rate.tsx
+++ frontend/src/components/home/Rate.tsx
@@ -1,11 +1,12 @@
 import { FaStar } from 'react-icons/fa'
 import { FaRegStar } from 'react-icons/fa'
 
-function Rate(props) {
+function Rate(props: { ratingDesc: string; rate: number, name: string }) {
   return (
     <div>
       <h1>{props.ratingDesc}</h1>
       <div>{props.rate ? <FaStar /> : <FaRegStar />}</div>
+      <h1>{props.name}</h1>
     </div>
   )
 }
