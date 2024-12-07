c967e8264aad30c7157046e3b4a2686da6f10d3c
josh <aaronbaon1@gmail.com>
2024-12-07 21:58:59 +0800

    add styling

--- frontend/src/components/home/RatingSection.tsx
+++ frontend/src/components/home/RatingSection.tsx
@@ -2,9 +2,11 @@ import RateCard from "./RateCard"
 
 function RatingSection() {
   return (
-    <div>
-      <h1>WHAT CLIENTS ARE SAYING</h1>
-      <div>
+    <div className="py-36">
+      <h1 className="text-center mb-32 text-3xl tracking-widest">WHAT CLIENTS ARE SAYING</h1>
+      <div className="flex gap-20 justify-center">
+        <RateCard ratingDesc="This is the best real state listing website" rate={5} name="MARIUS" />
+        <RateCard ratingDesc="This is the best real state listing website" rate={5} name="MARIUS" />
         <RateCard ratingDesc="This is the best real state listing website" rate={5} name="MARIUS" />
       </div>
     </div>
