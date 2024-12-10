bebebb8e921ed4a0820d7ea5d4b309b04578843c
josh <aaronbaon1@gmail.com>
2024-12-10 22:23:20 +0800

    refactor to use props for dynamic content

--- frontend/src/property/Details.tsx
+++ frontend/src/property/Details.tsx
@@ -12,7 +12,18 @@ import { MdLocalMall } from 'react-icons/md'
 import { IoMdRestaurant } from 'react-icons/io'
 import { MdOutlineLocalAirport } from 'react-icons/md'
 
-function Details() {
+interface PropsTypes {
+  title: string
+  location: string
+  price: string
+  bedroom: number
+  toilet: number
+  SwimmingPool: number
+  carPort: number
+  description: string
+}
+
+function Details(props: PropsTypes) {
   return (
     <div className="font-proximaNova">
       <div className="flex flex-row h-96">
@@ -26,28 +37,28 @@ function Details() {
       <div className="container mx-auto">
         <div className="flex flex-row justify-between items-center pt-10 pb-16 border-b border-beige-400">
           <div className="flex flex-col gap-5 w-4/5">
-            <h1 className="font-mogella text-3xl">LUXE HAVEN RESIDENCES</h1>
+            <h1 className="font-mogella text-3xl">{props.title}</h1>
             <div className="flex flex-row gap-3 items-center ">
               <FaLocationDot />
-              <h1>Sampaloc, Manila Philippines</h1>
+              <h1>{props.location}</h1>
             </div>
-            <h1>Php 13,500,000.00</h1>
+            <h1>{props.price}</h1>
             <div className="flex flex-row justify-between w-3/5  text-beige-400">
               <div className="flex flex-row gap-3 items-center">
                 <IoBedOutline />
-                <h1>Bedroom</h1>
+                <h1>{props.bedroom} Bedroom</h1>
               </div>
               <div className="flex flex-row gap-3 items-center">
                 <PiBathtubFill />
-                <h1>Toilet & Bath</h1>
+                <h1>{props.toilet} Toilet & Bath</h1>
               </div>
               <div className="flex flex-row gap-3 items-center">
                 <FaPersonSwimming />
-                <h1>Swimming Pool</h1>
+                <h1>{props.SwimmingPool} Swimming Pool</h1>
               </div>
               <div className="flex flex-row gap-3 items-center">
                 <FaCar />
-                <h1>Car Port</h1>
+                <h1>{props.carPort} Car Port</h1>
               </div>
             </div>
           </div>
@@ -60,23 +71,7 @@ function Details() {
         </div>
         <div>
           <p className="px-20 tracking-widest leading-10 text-lg py-32">
-            <span className="text-beige-400 font-mogella text-2xl">
-              Luxe Haven Residences
-            </span>{' '}
-            presents an extraordinary living experience, situated in the serene
-            neighborhood of Sampaloc, Manila. This modern masterpiece exudes
-            sophistication and comfort, featuring expansive spaces that
-            seamlessly blend indoor luxury with outdoor tranquility. With 5
-            spacious bedrooms, 4 elegantly designed bathrooms, a refreshing
-            swimming pool, and a 4-car port, this residence perfectly balances
-            form and function. The open-plan layout invites natural light,
-            highlighting high-end finishes and contemporary architecture
-            throughout. <br /> Enjoy moments of relaxation in the exquisite bath
-            area or host gatherings in the spacious living and dining spaces.
-            The property’s state-of-the-art amenities and lush surroundings
-            create an unparalleled ambiance that caters to both relaxation and
-            entertainment. Embrace the prestige of Luxe Haven Residences – where
-            luxury meets timeless elegance.
+            {props.description}
           </p>
         </div>
         <div className="flex flex-col py-28">
