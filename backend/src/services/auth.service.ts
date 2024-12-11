0b314f9444f9afe3c7bb1d02b35679cddd4f8b95
josh <aaronbaon1@gmail.com>
2024-12-11 18:23:38 +0800

    create AuthService for user registration

---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,14 @@
+import { Prisma, User } from "@prisma/client";
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService) { }
+
+    async register(data: Prisma.UserCreateInput): Promise<User> {
+        return this.prisma.user.create({
+            data
+        })
+    }
+}
