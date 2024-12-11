f6454104cbc0c4084db8273c08e2e24004e7c800
josh <aaronbaon1@gmail.com>
2024-12-11 21:23:07 +0800

    registration functionality and database migration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,18 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { AuthController } from "./controller/auth.controller";
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController, AuthController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule { }
--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { AuthService } from './../services/auth.service';
-import { Body, Controller, Post } from "@nestjs/common";
+
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
 import { User } from '@prisma/client';
 import { CreateUserDto } from "src/dto/auth.dto";
+import { AuthService } from "./auth.service";
 
 @Controller('auth')
 export class AuthController {
-
     constructor(private readonly authService: AuthService) { }
 
+    @HttpCode(201)
     @Post('register')
     async create(@Body() createUserDto: CreateUserDto): Promise<User> {
         const { username, email, name, password } = createUserDto;
