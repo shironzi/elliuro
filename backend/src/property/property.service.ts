40d1fd0ce0582a956f57557ac53c0684d3bbd9ea
josh <aaronbaon1@gmail.com>
2024-12-14 16:30:28 +0800

    add create method for creating property

--- backend/src/property/property.service.ts
+++ backend/src/property/property.service.ts
@@ -6,7 +6,33 @@ import { PropertyDto } from './property.dto';
 export class PropertyService {
     constructor(private prisma: PrismaService) { }
 
-    create(data = PropertyDto) {
-        
+    create(data: PropertyDto) {
+        return this.prisma.property.create({
+            data: {
+                ...data,
+                user: {
+                    connect: { id: 1 }
+                },
+                details: {
+                    create: data.details
+                },
+                Property_Images: {
+                    create: data.images.map(image => ({
+                        image: {
+                            create: image
+                        }
+                    }))
+                },
+                Property_Amenities: {
+                    create: data.amenities_list.map(amenity => ({
+                        amenity: {
+                            connect: { id: amenity.id }
+                        },
+                        value: amenity.value
+                    }))
+                }
+
+            }
+        })
     }
 }
