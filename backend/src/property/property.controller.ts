fc86cca94ca99525e7ff79a8026c51683d27247b
josh <aaronbaon1@gmail.com>
2024-12-14 18:36:18 +0800

    add create, update publish and delete methods

--- backend/src/property/property.controller.ts
+++ backend/src/property/property.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Post, Put } from '@nestjs/common';
 import { PropertyService } from './property.service';
+import { Property_Draft } from './property_draft.dto';
+import { Property_Publish } from './property_publish.dto';
 
 @Controller('property')
 export class PropertyController {
@@ -8,8 +10,23 @@ export class PropertyController {
     // Accept incomplete data and save the property with a status of `'draft'`.
 
     @Post('create')
-    async createProperty() {
+    async createProperty(@Body() data: Property_Draft) {
+        return this.propertyService.createDraft(data)
+    }
 
+    @Put('update')
+    async updateProperty(@Body() data: Property_Draft, property_id: number) {
+        return this.propertyService.update(data, property_id)
+    }
+
+    @Put('publish')
+    async publishProperty(@Body() data: Property_Publish, property_id: number) {
+        return this.propertyService.publish(data, property_id)
+    }
+
+    @Delete('delete')
+    async deleteProperty(property_id: number) {
+        return this.propertyService.delete(property_id)
     }
 
 }
