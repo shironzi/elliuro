640e426ddfc377437135236b63677647fdd34f79
josh <aaronbaon1@gmail.com>
2024-12-14 18:26:54 +0800

    add publish method for publishing property

--- backend/src/property/property.controller.ts
+++ backend/src/property/property.controller.ts
@@ -1,10 +1,15 @@
 import { Controller, Post } from '@nestjs/common';
+import { PropertyService } from './property.service';
 
 @Controller('property')
 export class PropertyController {
+    constructor(private readonly propertyService: PropertyService) { }
 
     // Accept incomplete data and save the property with a status of `'draft'`.
 
-    // @Post('id:/draft')
+    @Post('create')
+    async createProperty() {
+
+    }
 
 }
