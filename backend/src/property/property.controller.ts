c1d5a87d4eda9eb5bc2d251ea5522e0463cf4bce
josh <aaronbaon1@gmail.com>
2024-12-14 18:40:44 +0800

    update property methods to use route parameters for property_id

--- backend/src/property/property.controller.ts
+++ backend/src/property/property.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, Put } from '@nestjs/common';
+
 import { PropertyService } from './property.service';
 import { Property_Draft } from './property_draft.dto';
 import { Property_Publish } from './property_publish.dto';
@@ -7,25 +8,23 @@ import { Property_Publish } from './property_publish.dto';
 export class PropertyController {
     constructor(private readonly propertyService: PropertyService) { }
 
-    // Accept incomplete data and save the property with a status of `'draft'`.
-
     @Post('create')
     async createProperty(@Body() data: Property_Draft) {
         return this.propertyService.createDraft(data)
     }
 
     @Put('update')
-    async updateProperty(@Body() data: Property_Draft, property_id: number) {
+    async updateProperty(@Body() data: Property_Draft, @Param('id') property_id: number) {
         return this.propertyService.update(data, property_id)
     }
 
     @Put('publish')
-    async publishProperty(@Body() data: Property_Publish, property_id: number) {
+    async publishProperty(@Body() data: Property_Publish, @Param('id') property_id: number) {
         return this.propertyService.publish(data, property_id)
     }
 
     @Delete('delete')
-    async deleteProperty(property_id: number) {
+    async deleteProperty(@Param('id') property_id: number) {
         return this.propertyService.delete(property_id)
     }
 
