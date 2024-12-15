c05d41610af8b0a924e4ebefb8c44a5860b35f6c
josh <aaronbaon1@gmail.com>
2024-12-15 18:58:59 +0800

    improve validation and structure

--- backend/src/property/property_draft.dto.ts
+++ backend/src/property/property_draft.dto.ts
@@ -1,16 +1,6 @@
 import { Type } from "class-transformer";
-import { IsArray, IsDate, IsEnum, isInt, IsInt, IsJSON, IsNumber, IsOptional, IsString, ValidateNested } from "class-validator";
+import { IsEnum, IsInt, IsNumber, IsOptional, IsString, ValidateNested } from "class-validator";
 
-/**
- * Enum representing different types of properties.
- * 
- * @enum {string}
- * @property {string} House - Represents a house property.
- * @property {string} Apartment - Represents an apartment property.
- * @property {string} Hotel - Represents a hotel property.
- * @property {string} Condominium - Represents a condominium property.
- * @property {string} Private - Represents a private property.
- */
 enum PropertyType {
     HOUSE = 'HOUSE',
     Apartment = 'apartment',
@@ -26,82 +16,66 @@ enum StatusType {
     Active = "active"
 }
 
-class AmenityDto {
-    @IsOptional()
-    @IsInt()
-    id: number
-
-    @IsString()
-    name: string;
-
-    @IsNumber()
-    value: number;
-}
-
-class ImageDto {
-    @IsOptional()
-    @IsInt()
-    id: number
-
-    imageFile: File;
-
-    @IsString()
-    added_at: Date;
-}
-
-class DetailsDto {
-    @IsOptional()
-    @IsInt()
-    id: number
-
-    @IsOptional()
-    @IsString()
-    title: string;
-
-    @IsOptional()
-    @IsEnum(PropertyType)
-    type: PropertyType;
-
-    @IsOptional()
-    @IsString()
-    location: string;
-
-    @IsOptional()
-    @IsInt()
-    price: number;
-
-    @IsOptional()
-    @IsString()
-    description: string;
-}
-
 class StatusDto {
-    @IsOptional()
-    @IsInt()
-    id: number
-
     @IsEnum(StatusType)
     status: StatusType
 }
 
-export class Property_Draft {
+class DetailsDto {
+    @IsOptional()
+    @IsString()
+    title?: string
+
+    @IsEnum(PropertyType)
+    type: PropertyType
 
     @IsOptional()
-    @ValidateNested({ each: true })
+    @IsString()
+    location?: string
+
+    @IsOptional()
+    @IsNumber()
+    price?: number;
+
+    @IsOptional()
+    @IsString()
+    description?: string
+}
+
+class ImageDto {
+    @IsOptional()
+    @IsString()
+    image?: string
+
+    @IsOptional()
+    @IsString()
+    added_at?: string
+}
+
+class AmenityDto {
+    @IsOptional()
+    @IsString()
+    name: string;
+
+    @IsOptional()
+    @IsNumber()
+    value: number
+}
+
+export class PropertyDraftDto {
+    @ValidateNested()
+    @Type(() => StatusDto)
+    status: StatusDto;
+
+    @ValidateNested()
     @Type(() => DetailsDto)
     details: DetailsDto;
 
     @ValidateNested({ each: true })
-    @Type(() => StatusDto)
-    status: StatusDto
+    @Type(() => ImageDto)
+    images: ImageDto[];
 
-    @IsOptional()
     @ValidateNested({ each: true })
     @Type(() => AmenityDto)
     amenities: AmenityDto[];
-
-    @IsOptional()
-    @ValidateNested({ each: true })
-    @Type(() => ImageDto)
-    images: ImageDto[];
 }
