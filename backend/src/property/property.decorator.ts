d8cecc80f0d841e9bc5c7fa2aa5ef8943c7ca730
josh <aaronbaon1@gmail.com>
2024-12-16 15:37:01 +0800

    improve checks for details and amenities

--- backend/src/property/property.decorator.ts
+++ backend/src/property/property.decorator.ts
@@ -1,16 +1,16 @@
 import { BadRequestException, createParamDecorator, ExecutionContext } from "@nestjs/common"
-import { Property_Publish } from "./property_publish.dto"
+import { PropertyPublishDto } from "./property_publish.dto"
 
 export const ValidatePublishData = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest();
-        const body: Property_Publish = request.body;
+        const body: PropertyPublishDto = request.body;
 
-        if (!body.details || body.details.length === 0) {
+        if (!body.details || Object.keys(body.details).length === 0) {
             throw new BadRequestException('Details are required for publishing a property.');
         }
 
-        if (!body.amenities_list || body.amenities_list.length === 0) {
+        if (!body.amenities || Object.keys(body.amenities).length === 0) {
             throw new BadRequestException('Amenities are required for publishing a property.');
         }
 
