d6ec17e913c1ac94bfef2ef3f50c46db4c1a5231
josh <aaronbaon1@gmail.com>
2024-12-22 01:12:38 +0800

    feat: add PropertyCard component and integrate it into ExploreProperties

--- frontend/src/components/exploreProperties/ExploreProperties.tsx
+++ frontend/src/components/exploreProperties/ExploreProperties.tsx
@@ -1,9 +1,10 @@
 import { IoIosSearch } from 'react-icons/io'
+import PropertyCard from './propertyCard'
 
 function ExploreProperties() {
   return (
     <div className="bg-darkGray h-full">
-      <div className="container">
+      <div className="container mx-auto">
         <form
           action=""
           className="border border-secondary-400 rounded-full border-r-0 my-20 w-fit mx-auto pl-5 flex flex-row items-center justify-center space-x-4 text-xl text-beige-400"
@@ -28,14 +29,48 @@ function ExploreProperties() {
             <IoIosSearch size={35} color="ffffff" />
           </button>
         </form>
-        <div>
-            <div>
-                <h1>0 PROPERTIES</h1>
-                <div>
-                    
-                </div>
+        <div className='flex flex-row my-32 '>
+          <div className='w-fit'>
+            <h1>0 PROPERTIES</h1>
+            <div className='grid grid-cols-4 gap-10 place-items-center'>
+              <PropertyCard
+                title="title"
+                location="manila"
+                price={150000}
+                size="1 hectare"
+              />
+              <PropertyCard
+                title="title"
+                location="manila"
+                price={150000}
+                size="1 hectare"
+              />
+              <PropertyCard
+                title="title"
+                location="manila"
+                price={150000}
+                size="1 hectare"
+              />
+              <PropertyCard
+                title="title"
+                location="manila"
+                price={150000}
+                size="1 hectare"
+              />
+              <PropertyCard
+                title="title"
+                location="manila"
+                price={150000}
+                size="1 hectare"
+              />
+              <PropertyCard
+                title="title"
+                location="manila"
+                price={150000}
+                size="1 hectare"
+              />
             </div>
-            <div></div>
+          </div>
         </div>
       </div>
     </div>
---
+++ frontend/src/components/exploreProperties/PropertyCard.tsx
@@ -0,0 +1,30 @@
+import { FaLocationDot } from 'react-icons/fa6'
+
+function PropertyCard(props: {
+  title: string
+  location: string
+  price: number
+  size: string
+}) {
+  const formattedPrice = new Intl.NumberFormat('en-PH', {
+    minimumFractionDigits: 2,
+  }).format(props.price)
+  return (
+    <div className="text-darkGray-400 font-proximaNova w-fit">
+      <img
+        src="/public/images/property-card.jpg"
+        alt=""
+        className="w-full object-cover rounded-t-lg"
+      />
+      <h1>{props.title}</h1>
+      <h1 className="text-sm flex flex-row gap-2 items-center w-fit">
+        <FaLocationDot />
+        {props.location}
+      </h1>
+      <h1 className="text-sm w-fit">Php {formattedPrice}</h1>
+      <h1 className="text-xs w-fit">{props.size}</h1>
+    </div>
+  )
+}
+
+export default PropertyCard
