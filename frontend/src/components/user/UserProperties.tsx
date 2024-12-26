dde2b379e2a3c65400088931190b861faf30ebe9
shironzi <aaronbaon1@gmail.com>
2024-12-26 14:08:45 +0800

    refactor: rename Properties component to UserProperties and update header label

--- frontend/src/components/user/UserProperties.tsx
+++ frontend/src/components/user/UserProperties.tsx
@@ -1,13 +1,13 @@
 import { MdOutlineEdit } from 'react-icons/md'
 import { MdDeleteOutline } from 'react-icons/md'
 
-function Properties() {
+function userProperties() {
   return (
     <div className="bg-darkGray-400">
       <div className="container mx-auto py-20">
         <div className="min-h-80 bg-transparent font-proximaNova border rounded-3xl overflow-hidden py-5 px-7">
           <div className="grid grid-cols-10 font-bold text-center py-5 text-md">
-            <div>ID</div>
+            <div>No.</div>
             <div className="col-span-2">Image</div>
             <div className="col-span-6">Title</div>
             <div className="col-span-1">Actions</div>
@@ -37,4 +37,4 @@ function Properties() {
   )
 }
 
-export default Properties
+export default userProperties
